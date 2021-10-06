d468c8e31527e8ff9a7dfdfe5791da3c593ccf69
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-06 20:18:32 +0300

    It will add a goals class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,11 @@ import { Goals } from './goals';
 export class AppComponent {
   title = 'My Goals';
   goals: Goals[] = [
-    {id:1, name:'Watch finding Nemo',description:'Find an online version and watch merlin find his son'},
-    {id:2,name:'Buy Cookies',description:'I have to buy cookies for the parrot'},
-    {id:3,name:'Get new Phone Case',description:'Diana has her birthday coming up soon'},
-    {id:4,name:'Get Dog Food',description:'Pupper likes expensive sancks'},
-    {id:5,name:'Solve math homework',description:'Damn Math'},
-    {id:6,name:'Plot my world domination plan',description:'Cause I am an evil overlord'},
+    {id:1, name:'Watch finding Nemo'},
+    {id:2,name:'Buy Cookies'},
+    {id:3,name:'Get new Phone Case'},
+    {id:4,name:'Get Dog Food'},
+    {id:5,name:'Solve math homework'},
+    {id:6,name:'Plot my world domination plan'},
   ];
 }
--- src/app/goals.ts
+++ src/app/goals.ts
@@ -1,5 +1,4 @@
 export class Goals {
     id!: number;
     name!: string;
-    description:  string;
   }
