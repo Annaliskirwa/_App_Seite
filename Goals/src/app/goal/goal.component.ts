94606c1bf057badf739964f530cd91e8507307a3
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-07 22:16:14 +0300

    Emmiting events

--- Goals/src/app/goal/goal.component.ts
+++ Goals/src/app/goal/goal.component.ts
@@ -19,6 +19,12 @@ export class GoalComponent implements OnInit {
    toggleDetails(index: number){
     this.goals[index].showDescription = !this.goals[index].showDescription;
   }
+
+  completeGoal(isComplete:any, index:number){
+    if (isComplete) {
+      this.goals.splice(index,1);
+    }
+  }
   constructor() { }
 
   ngOnInit(): void {
