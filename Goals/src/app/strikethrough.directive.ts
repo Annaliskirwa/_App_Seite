ee4b2c5556d07e348328627f6ab01947d5867463
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-08 00:16:03 +0300

    ElementRef to inject a reference to the host DOM element in which we will use this directive.

--- Goals/src/app/strikethrough.directive.ts
+++ Goals/src/app/strikethrough.directive.ts
@@ -1,10 +1,10 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef} from '@angular/core';
 
 @Directive({
   selector: '[appStrikethrough]'
 })
 export class StrikethroughDirective {
 
-  constructor() { }
+  constructor(private elem:ElementRef) { }
 
 }
