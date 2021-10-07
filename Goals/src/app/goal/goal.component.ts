a3c1a6f2517b49b5ae9edd428d3525861cf01b22
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-07 21:14:09 +0300

    It will nest child components using input property binding

--- Goals/src/app/goal/goal.component.ts
+++ Goals/src/app/goal/goal.component.ts
@@ -8,12 +8,12 @@ import { Goal } from '../goal';
 })
 export class GoalComponent implements OnInit {
   goals:Goal[] = [
-    {id:1, name: "Watch finding Nemo"},
-    {id:2, name: "I should buy cookies"},
-    {id:3, name: "Finish QA"},
-    {id:4, name: "Do UX deisgn chanbges"},
-    {id:5, name: "Call someone"},
-    {id:6, name: "Scream at midnught"}
+    {id:1, name: "Watch finding Nemo", description: "It should be a good movie"},
+    {id:2, name: "I should buy cookies", description: "AAh nice"},
+    {id:3, name: "Finish QA", description: "Because someone is going to be om my neck"},
+    {id:4, name: "Do UX deisgn chanbges", description: "Just upskill"},
+    {id:5, name: "Call someone", description: "Prolly mum"},
+    {id:6, name: "Scream at midnught", description: "I am tired"}
   ];
 
   constructor() { }
