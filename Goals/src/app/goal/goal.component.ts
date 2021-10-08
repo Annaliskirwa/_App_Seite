6bbaf3d2fa32a628d4d6349765b73f68507dad94
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-08 10:19:59 +0300

    It will confirm the deleting of goals, using splice

--- Goals/src/app/goal/goal.component.ts
+++ Goals/src/app/goal/goal.component.ts
@@ -20,9 +20,13 @@ export class GoalComponent implements OnInit {
     this.goals[index].showDescription = !this.goals[index].showDescription;
   }
 
-  completeGoal(isComplete:any, index:number){
+  deleteGoal(isComplete:any, index:number){
     if (isComplete) {
-      this.goals.splice(index,1);
+      let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
+
+      if (toDelete){
+        this.goals.splice(index,1)
+      }
     }
   }
   constructor() { }
