87901132958038304722520bf46040b52284f6f5
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-08 17:41:18 +0300

    UHuhu, it works at last

--- Goals/src/app/goal/goal.component.ts
+++ Goals/src/app/goal/goal.component.ts
@@ -8,15 +8,14 @@ import { Goal } from '../goal';
 })
 export class GoalComponent implements OnInit {
   goals:Goal[] = [
-    new Goal(1,  "Watch finding Nemo",  "It should be a good movie", new Date(2021,10,8)),
-    new Goal(2,  "I should buy cookies",  "AAh nice", new Date(2021,11,08)),
-    new Goal(3,  "Finish QA", "Because someone is going to be om my neck", new Date(2021,9,11)),
-    new Goal(4,  "Do UX deisgn chanbges",  "Just upskill", new Date(2021,7,12)),
-    new Goal(5, "Call someone", "Prolly mum", new Date(2021,5,15)),
-    new Goal(6,  "Scream at midnught",  "I am tired", new Date(2021,4,16))
-  ];
+    new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2020,3,14)),
+    new Goal(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
+    new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2022,1,12)),
+    new Goal(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,0,18)),
+    new Goal(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
+    new Goal(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),
 
-  
+  ];
    toggleDetails(index: number){
     this.goals[index].showDescription = !this.goals[index].showDescription;
   }
