098b9581e0372a239bbec5e45318f5e2d4b5c52a
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-12 14:23:46 +0300

    Registering a service In the @component() metadata

--- Goals/src/app/goal/goal.component.ts
+++ Goals/src/app/goal/goal.component.ts
@@ -5,7 +5,9 @@ import { Goal } from '../goal';
 @Component({
   selector: 'app-goal',
   templateUrl: './goal.component.html',
-  styleUrls: ['./goal.component.css']
+  styleUrls: ['./goal.component.css'],
+  //Registering a service In the @component() metadata
+  providers: [GoalService]
 })
 export class GoalComponent implements OnInit {
   addNewGoal(goal: any){
