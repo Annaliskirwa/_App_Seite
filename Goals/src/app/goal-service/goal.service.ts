384b35930c319555fd69e14c5b4989c0bdfad7bc
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-12 13:49:13 +0300

    It makes the service access goals

--- Goals/src/app/goal-service/goal.service.ts
+++ Goals/src/app/goal-service/goal.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Goals } from '../goalList';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GoalService {
+
+  getGoals(){
+    return Goals
+  }
 
   constructor() { }
 }
