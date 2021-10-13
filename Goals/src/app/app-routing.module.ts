8d8a916dcb214238b6ef7431b2dd2e14d96abf7e
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-13 08:49:19 +0300

    It will add routes for paths not found

--- Goals/src/app/app-routing.module.ts
+++ Goals/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { GoalComponent } from './goal/goal.component';
 import { AboutComponent } from './about/about.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 const routes: Routes = [
   { path: 'goals', component: GoalComponent},
   { path: 'about', component: AboutComponent},
+  { path:'**', component:NotFoundComponent},
 ];
 
 @NgModule({
