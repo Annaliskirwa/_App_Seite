74bd4c2d6f04ab9086d6322005519f311bd60207
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-13 08:45:42 +0300

    It will generate a 404 page component

--- Goals/src/app/app.module.ts
+++ Goals/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DateCountPipe } from './date-count.pipe';
 import { GoalFormComponent } from './goal-form/goal-form.component';
 import { AboutComponent } from './about/about.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { NavbarComponent } from './navbar/navbar.component';
     GoalFormComponent,
     AboutComponent,
     NavbarComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
