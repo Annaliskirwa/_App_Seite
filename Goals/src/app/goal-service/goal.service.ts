c6d61660b0b9c0c4e4a38d752dddf03beb6857d7
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-13 19:00:45 +0300

    a method in the service that will help retrieve one goal.

--- Goals/src/app/goal-service/goal.service.ts
+++ Goals/src/app/goal-service/goal.service.ts
@@ -9,6 +9,12 @@ export class GoalService {
   getGoals(){
     return Goals
   }
-
+  getGoal(id:any){
+    for (let goal of Goals){
+      if (goal.id == id){
+        return goal;
+      }
+    }
+  }
   constructor() { }
 }
