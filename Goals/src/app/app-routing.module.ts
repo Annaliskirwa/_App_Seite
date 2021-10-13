7b0edc8ede5d8997ca34725f4a76f4feb268c138
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-13 18:37:12 +0300

    It will create a route that maps to a specific goal

--- Goals/src/app/app-routing.module.ts
+++ Goals/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { GoalComponent } from './goal/goal.component';
 import { AboutComponent } from './about/about.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { GoalDetailComponent } from './goal-detail/goal-detail.component';
 
 const routes: Routes = [
   { path: 'goals', component: GoalComponent},
   { path: 'about', component: AboutComponent},
+  { path: 'goals/:id', component: GoalDetailComponent },
   { path:'**', component:NotFoundComponent},
 
   { path: '', redirectTo:"/goals", pathMatch:"full"},
