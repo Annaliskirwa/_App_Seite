4fd64a944e201a298ea3edf89758e8d736b178bb
Annaliskirwa <annaliskirwa@gmail.com>
2021-10-13 18:54:15 +0300

    It will adjust the deletegoal function

--- Goals/src/app/goal/goal.component.ts
+++ Goals/src/app/goal/goal.component.ts
@@ -33,14 +33,12 @@ export class GoalComponent implements OnInit {
     this.goals[index].showDescription = !this.goals[index].showDescription;
   }
 
-  deleteGoal(isComplete:any, index:number){
-    if (isComplete) {
-      let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
+  deleteGoal(index:any){
+    let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}`)
 
-      if (toDelete){
-        this.goals.splice(index,1)
-        this.alertService.alertMe("The goal has been deleted")
-      }
+    if (toDelete){
+      this.goals.splice(index,1)
+      this.alertService.alertMe("Goal has been deleted")
     }
   }
   constructor(goalService:GoalService, alertService:AlertService, private http:HttpClient, private quoteService:QuoteRequestService, private router:Router) { 
